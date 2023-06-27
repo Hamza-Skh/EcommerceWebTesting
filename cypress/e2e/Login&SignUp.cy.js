74b009704b2ad9edf778e7300632c9d26a940588
Hamza-Skh <hamzayounas1994@outlook.com>
2023-06-27 13:43:00 +0500

    all test cases

--- cypress/e2e/Login&SignUp.cy.js
+++ cypress/e2e/Login&SignUp.cy.js
@@ -11,38 +11,14 @@ describe('This suite contain test cases of login and signup', () => {
         Home.Login_SignUPBtn.click()
         cy.UserSignUp((Cypress.env('Name')), (Cypress.env('email')))
         cy.NewUserRegistration()
-        //cy.Del_Account()
+        cy.Del_Account()
     })
-    it('Verify user login sucessfully after entering correct credentials', () => {
-        cy.login((Cypress.env('email')), (Cypress.env('password')))
-        //cy.Del_Account()
-    })
-    it('Verify user does not login with incorrect password', () => {
-        Home.launch()
-        Home.Login_SignUPBtn.click()
-        cy.get('.login-form h2').should('have.text', 'Login to your account')
-        cy.get('[data-qa="login-email"]').type(Cypress.env('email'))
-        CommonObject.PasswordField.type('invalidpassword')
-        cy.get('[data-qa="login-button"]').click()
-        cy.get('.login-form > form > p').should('have.text', 'Your email or password is incorrect!')
-    })
-    it('Verify user logout sucessfully', () => {
-        cy.login((Cypress.env('email')), (Cypress.env('password')))
-        Home.LogOutBn.click();
-        cy.get('.login-form').should('exist');
-    })
-    it('Verify user get an error on Registering User with an existing email', () => {
-        Home.launch()
-        Home.Login_SignUPBtn.click()
-        cy.UserSignUp((Cypress.env('Name')), (Cypress.env('email')))
-        cy.get('.signup-form > form > p').should('have.text', 'Email Address already exist!')
-    })
-    it.skip('Verfiy user can signup while checkout', () => {
+    it('Verfiy user can signup while checkout', () => {
         cy.ProductPageVisit()
         cy.AddProductsInCart()
         cy.get('.col-sm-6 > .btn').click()
         Home.Login_SignUPBtn.eq(1).click()
-        cy.UserSignUp('Hamza', (Cypress.env('email')))
+        cy.UserSignUp((Cypress.env('Name')), (Cypress.env('email')))
         cy.NewUserRegistration()
         Home.CartBtn.click()
         cy.PlaceAnOrder()
@@ -50,20 +26,11 @@ describe('This suite contain test cases of login and signup', () => {
         //cy.get('.alert-success.alert').should('be.visible').contains('Your order has been placed successfully!');
         cy.Del_Account()
     })
-    it.skip('Verify user checkout after signup', () => {
+    it('Verify user checkout after signup', () => {
         Home.launch()
         Home.Login_SignUPBtn.click()
-        cy.UserSignUp('Hamza', (Cypress.env('email')))
+        cy.UserSignUp((Cypress.env('Name')), (Cypress.env('email')))
         cy.NewUserRegistration()
-        cy.get('.productinfo.text-center').eq(0).trigger('mouseover').find('.add-to-cart').click({ multiple: true })
-        cy.get('.modal-footer > .btn').should('be.visible').click()
-        Home.CartBtn.click()
-        cy.get('#product-1 > .cart_product > a > .product_image').should('be.visible')
-        cy.PlaceAnOrder()
-        cy.Del_Account()
-    })
-    it('Verify user place an order after login', () => {
-        cy.login((Cypress.env('email')), (Cypress.env('password')))
         cy.get('.productinfo.text-center').eq(0).trigger('mouseover').find('.add-to-cart').click({ multiple: true })
         cy.get('.modal-footer > .btn').should('be.visible').click()
         Home.CartBtn.click()
@@ -74,32 +41,77 @@ describe('This suite contain test cases of login and signup', () => {
     it("Verify address details are correct in checkout page", () => {
         Home.launch()
         Home.Login_SignUPBtn.click()
-        cy.UserSignUp('Hamza', (Cypress.env('email')))
+        cy.UserSignUp((Cypress.env('Name')), (Cypress.env('email')))
         cy.NewUserRegistration()
         cy.ProductPageVisit()
         cy.AddProductsInCart()
         CommonObject.OrderCheckOutBtn.click()
         cy.get('li.address_address1.address_address2').eq(1).should('have.text', Cypress.env('CustomerAddress'))
         cy.get('li.address_address1.address_address2').eq(4).should('have.text', Cypress.env('CustomerAddress'))
+    })
+    it('Verify user does not login with incorrect password', () => {
+        Home.launch()
+        Home.Login_SignUPBtn.click()
+        cy.get('.login-form h2').should('have.text', 'Login to your account')
+        cy.get('[data-qa="login-email"]').type(Cypress.env('email'))
+        CommonObject.PasswordField.type('invalidpassword')
+        cy.get('[data-qa="login-button"]').click()
+        cy.get('.login-form > form > p').should('have.text', 'Your email or password is incorrect!')
+    })
+    it('Verify user login sucessfully after entering correct credentials', () => {
+        cy.login((Cypress.env('email')), (Cypress.env('password')))
+    })
+    it('Verify user logout sucessfully', () => {
+        cy.login((Cypress.env('email')), (Cypress.env('password')))
+        Home.LogOutBn.click();
+        cy.get('.login-form').should('exist');
+    })
+    it('Verify user Search Products and Verify Cart After Login', () => {
+        cy.ProductPageVisit()
+        cy.SearchProduct()
+        cy.get('.productinfo.text-center').eq(0).trigger('mouseover').find('.add-to-cart').click({ multiple: true })
+        CommonObject.ViewCart.click();
+        cy.login((Cypress.env('email')), (Cypress.env('password')))
+        Home.CartBtn.click()
+        cy.get('#product-2 > .cart_product > a > .product_image').should('be.visible')
+    })
+    it('Verify user get an error on Registering User with an existing email', () => {
+        Home.launch()
+        Home.Login_SignUPBtn.click()
+        cy.UserSignUp((Cypress.env('Name')), (Cypress.env('email')))
+        cy.get('.signup-form > form > p').should('have.text', 'Email Address already exist!')
+    })
+    it('Verify user place an order after login', () => {
+        cy.login((Cypress.env('email')), (Cypress.env('password')))
+        cy.get('.productinfo.text-center').eq(0).trigger('mouseover').find('.add-to-cart').click({ multiple: true })
+        cy.get('.modal-footer > .btn').should('be.visible').click()
+        Home.CartBtn.click()
+        cy.get('#product-1 > .cart_product > a > .product_image').should('be.visible')
+        cy.PlaceAnOrder()
         cy.Del_Account()
     })
-    it("Verify user can Download Invoice after purchase order", () => {
-        cy.login((Cypress.env('email')), (Cypress.env('password')))
+    it.only("Verify user can Download Invoice after purchase order", () => {
+        Home.launch()
         Home.Login_SignUPBtn.click()
-        cy.UserSignUp('Hamza', (Cypress.env('email')))
+        cy.UserSignUp((Cypress.env('Name')), (Cypress.env('email')))
         cy.NewUserRegistration()
-        cy.UserNameVisibility()
         cy.ProductPageVisit()
-        cy.visit('https://www.automationexercise.com/payment_done/500')
         cy.AddProductsInCart()
         cy.PlaceAnOrder()
-        downloadFile();
-        cy.pause()
-        cy.get('.btn-default').contains('Download Invoice').click()
-            cy.pause()
-            cy.request('GET', 'https://www.automationexercise.com/download_invoice/*').then((response) => {
-                expect(response.status).to.eq(200)
-              })
+        cy.window().document().then(function (doc) {
+            doc.addEventListener('click', () => {
+              setTimeout(function () { doc.location.reload() }, 5000)
+            })
+            cy.get('.btn-default').contains('Download Invoice').click()
+          })
+        // cy.intercept('GET', '/Users/hamzayounas/Desktop/Testing E-Commerce Web/cypress/downloads', '').as('download')
+        // cy.visit('https://www.automationexercise.com/payment_done/900')
+        // cy.get('.btn-default').contains('Download Invoice').click()
+        // cy.wait('@download')
+         cy.readFile('/Users/hamzayounas/Desktop/Testing E-Commerce Web/cypress/downloads/invoice.txt').should('exist');
+            // cy.request('GET', 'https://www.automationexercise.com/download_invoice/*').then((response) => {
+            //     expect(response.status).to.eq(200)
+            //   })
         cy.Del_Account()
     })
 })
